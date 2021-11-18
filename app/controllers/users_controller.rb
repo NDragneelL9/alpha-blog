9829144479b84574333b869f1f8c77b0638320af
NdragneelL <nell.dragneel@gmail.com>
2021-11-18 14:02:19 +0300

    Added delete user and his articles functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
     end
   end
 
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -38,6 +37,13 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account successfully deleted"
+    redirect_to root_path
   end
 
   private
