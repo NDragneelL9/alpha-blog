21d917968ff5673a8e3ef2c1e18997cb374046e0
NdragneelL <nell.dragneel@gmail.com>
2021-11-20 22:50:49 +0300

    Add edit & destroy functionality to categories With admin user requirement

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, except: [:create, :new, :index ]
   
   def new
     @category = Category.new
@@ -15,16 +16,37 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category name succesfully updated"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:notice] = "Category successfully deleted"
+    redirect_to categories_path
+  end
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private 
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'about', to: "pages#about"
   resources :articles
+  resources :categories
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   get 'signin', to: 'sessions#new'
   post 'signin', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :categories, except: [:destroy]
 end
